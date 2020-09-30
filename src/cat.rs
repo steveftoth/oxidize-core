58876613f0db9e672aaa0b6b1cd3aa00c672ec74
Steve Toth <steve@toths.info>
2020-09-30 13:11:07 -0700

    Getting command line params to accept all the parameters in var args.

--- src/cat.rs
+++ src/cat.rs
@@ -1,12 +1,16 @@
-use clap::{Arg, App};
+use clap::{Arg, App, AppSettings};
 
 fn main() {
   let matches = App::new("cat")
+      .setting(AppSettings::TrailingVarArg)
       .version("0.1.0")
       .author("Steven Toth <steve@toths.info>")
       .about("Reimplementation of cat in rust")
       .arg(Arg::with_name("show-ends").short("E").long("show-ends").takes_value(false).help("display $ at end of each line"))
+      .arg(Arg::with_name("xtra").index(1).multiple(true))
       .get_matches();
 
-
+  let files: Vec<&str> = matches.values_of("xtra").unwrap().collect();
+  println!("{}",files.join(",")
+  );
 }
